ee81dde553d5a3f04b4094ddc53ccef6f243d4f5
김태운 <jim073@naver.com>
2024-04-24 16:05:52 +0900

    feat: frontend 기본 세팅

---
+++ frontend/sw.js
@@ -0,0 +1,17 @@
+//sw.js
+
+//install
+self.addEventListener("install", (event) => {
+    console.log("[Service Worker] installed");
+    console.log(event);
+});
+
+// activate event
+self.addEventListener("activate", (event) => {
+    console.log("[Service Worker] activated", event);
+});
+
+//fetch event
+self.addEventListener("fetch", (event) => {
+    console.log("[Service Worker] fetched resource " + event.request.url);
+})
