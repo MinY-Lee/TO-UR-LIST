265a51fa16c21f8979819677aa21d14b2cf0715e
김태운 <jim073@naver.com>
2024-04-25 11:00:55 +0900

    feat: 로컬 폰트 추가 설치 및 css variables 추가

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,17 @@
 export default function LoginPage() {
     return (
         <>
-            <h1 className="text-blue-500">TO-UR-LIST</h1>
-            <h5 className="bg-blue-500">여행의 처음부터 끝까지!</h5>
+            <h1
+                className="color-text-blue-1"
+                style={{ fontFamily: 'TTHakgyoansimSamulhamR' }}
+            >
+                TO-UR-LIST
+            </h1>
+            <h5 className="border-2 color-border-blue-1">
+                여행의 처음부터 끝까지!
+            </h5>
             <a href="/main">구글 아이디로 로그인</a>
+            <p className="text-xl weight-text-medium">hello</p>
         </>
     );
 }
