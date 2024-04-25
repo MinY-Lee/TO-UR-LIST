52f77964b77ca83d3568017c62015ccd7acef329
김태운 <jim073@naver.com>
2024-04-25 13:58:01 +0900

    feat: 로그인 페이지 1차 완성

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,17 +1,55 @@
+import loginImage from '../assets/image/loginPage.jpg';
+import googleLogin from '../assets/image/googleLogin.png';
+
 export default function LoginPage() {
     return (
         <>
-            <h1
-                className="color-text-blue-1"
-                style={{ fontFamily: 'TTHakgyoansimSamulhamR' }}
+            <section
+                style={{
+                    backgroundImage: `url(${loginImage})`,
+                    backgroundRepeat: `no-repeat`,
+                    backgroundPosition: `center`,
+                    backgroundSize: 'cover',
+                    backgroundAttachment: 'fixed',
+                }}
+                className="w-full h-full"
             >
-                TO-UR-LIST
-            </h1>
-            <h5 className="border-2 color-border-blue-1">
-                여행의 처음부터 끝까지!
-            </h5>
-            <a href="/main">구글 아이디로 로그인</a>
-            <p className="text-xl weight-text-medium">hello</p>
+                <div
+                    className="w-full h-full absolute top-0 left-0 bg-white opacity-[75%]"
+                    style={{ zIndex: 0 }}
+                ></div>
+                <div className="z-10 absolute top-0 left-0 w-full h-full flex flex-col justify-center">
+                    <div className="w-full h-[40%] flex flex-col justify-between items-center">
+                        <div className="flex flex-col items-center">
+                            <h1
+                                className="color-text-blue-1 text-[10vw]"
+                                style={{ fontFamily: 'TTHakgyoansimSamulhamR' }}
+                            >
+                                TO-UR-LIST
+                            </h1>
+                            <h5
+                                className="color-text-blue-1 text-[6vw]"
+                                style={{ fontFamily: 'TTHakgyoansimSamulhamR' }}
+                            >
+                                여행의 처음부터 끝까지!
+                            </h5>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <p className="text-[#565656] text-[5vw]">
+                                구글 아이디로 편리하게 로그인하세요!
+                            </p>
+                            <div
+                                className="w-[60%]"
+                                onClick={() => {
+                                    window.location.href = './main';
+                                }}
+                            >
+                                <img src={`${googleLogin}`} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
