ae0f321b8856a770b4c304639a1a964dc09903e4
유혜승 <h103906@gmail.com>
2024-04-30 18:03:41 +0900

    feat: 기본정보 디자인 수정

--- frontend/src/components/TourPage/MySlider.tsx
+++ frontend/src/components/TourPage/MySlider.tsx
@@ -14,24 +14,24 @@ export default function MySlider({ countryInfoList }) {
     <Slider {...settings}>
       {countryInfoList.length > 0 && countryInfoList.map((countryInfo, index) => (
           <div key={index} className="border-2 border-blue-200 rounded-2xl p-3">
-              <div className="grid grid-cols-3">
+              <div className="grid grid-cols-3 mb-3">
                   <div className="flex flex-col items-center">
                       <div className="text-gray-600">언어</div>
-                      <div className="text-center whitespace-pre">{ countryInfo.language.split(',').join('\n') }</div>
+                      <div className="text-lg whitespace-pre text-center">{ countryInfo.language.split(',').join('\n') }</div>
                   </div>
                   <div className="flex flex-col items-center">
                       <div className="text-gray-600">전압</div>
-                      <div>{ countryInfo.voltage.split(',').join('\n') }</div>
+                      <div className="text-lg whitespace-pre text-center">{ countryInfo.voltage.split(',').join('\n') }</div>
                   </div>
                   <div className="flex flex-col items-center">
                       <div className="text-gray-600">시차(한국기준)</div>
-                      <div>{ countryInfo.KST }시간</div>
+                      <div className="text-lg">{ countryInfo.KST }시간</div>
                   </div>
               </div>
               <div>
                 <div className="flex flex-col items-center">
                     <div className="text-gray-600">기후</div>
-                    <div className="text-wrap">{ countryInfo.climate }</div>
+                    <div className="text-lg text-wrap">{ countryInfo.climate }</div>
                 </div>
               </div>
             <div>
