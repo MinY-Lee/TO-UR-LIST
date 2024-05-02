564b4238521342b0ad286e01503242e7fa6c4ceb
김태운 <jim073@naver.com>
2024-05-02 09:32:16 +0900

    proceeding: 날짜 선택 바

--- frontend/src/pages/Schedule/TourSchedulePage.tsx
+++ frontend/src/pages/Schedule/TourSchedulePage.tsx
@@ -1,4 +1,6 @@
 import TabBarTour from '../../components/TabBar/TabBarTour';
+import DaySelectBar from '../../components/SchedulePage/DaySelectBar';
+import tourInfo from '../../dummy-data/get_tour_tourId.json';
 
 export default function TourSchedulePage() {
     // 투어 아이디 불러오기
@@ -10,6 +12,7 @@ export default function TourSchedulePage() {
             <h1>여행 일정 페이지</h1>
             <p>여행 일정을 정하는 페이지입니다.</p>
             <p>id : {tourId}</p>
+            <DaySelectBar />
             <TabBarTour tabMode={2} tourMode={2} tourId={tourId} />
         </>
     );
