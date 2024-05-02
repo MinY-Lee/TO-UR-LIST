6feeb55484b7a173a9aa7a0f38c1ab5cf0c10a09
유혜승 <h103906@gmail.com>
2024-05-02 18:10:49 +0900

    feat: 전체 수정 페이지 데이터 로드

--- frontend/src/pages/Checklist/ChecklistEditAllPage.tsx
+++ frontend/src/pages/Checklist/ChecklistEditAllPage.tsx
@@ -1,13 +1,54 @@
+import { useState, useEffect } from "react";
+
+import MyButton from "../../components/Buttons/myButton";
+import HeaderBar from "../../components/HeaderBar/HeaderBar";
+import { Item } from "../../types/types";
+
+import Checklist from '../../dummy-data/get_checklist.json'
+
+
 export default function ChecklistEditAllPage() {
     // 투어 아이디 불러오기
     const address: string[] = window.location.href.split('/');
-    const tourId: string = address[address.length - 3];
+    
+    const [data, setData] = useState<Item[]>([]);
+    const [tourId, setTourId] = useState<string>("");
+    
+    
+    useEffect(() => {
+        setTourId(address[address.length - 3]);
+
+        // 투어 아이디로 더미데이터에서 데이터 찾기 (임시)
+        const checkListData : Item[] = Checklist.filter(checklist => checklist.tourId === tourId);
+        if (checkListData) {
+            setData(checkListData);
+        }
+
+    }, [data]);
+
+    const handleDone = () => {
+
+    }
 
     return (
         <>
-            <h1>체크리스트 전체보기 수정 페이지</h1>
-            <p>체크리스트 전체보기에서 수정하는 페이지입니다.</p>
-            <p>tourId: {tourId}</p>
+            <header>
+                <HeaderBar/>
+            </header>
+            <div className="m-5">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-3xl font-bold">전체 체크리스트</h1>
+                    <MyButton className="text-2xl" isSelected={true} onClick={() => handleDone()} text="완료" type="small"/>
+
+                </div>
+                <div>
+                    {data.map((item, index) => (
+                        <div>
+                            {item.item}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
