b267d13ed8f44568bfba2a6fbde496b1dd6c6f5c
유혜승 <h103906@gmail.com>
2024-05-07 18:18:10 +0900

    feat: 가계부 탭 틀 잡기

--- frontend/src/pages/Account/AccountPage.tsx
+++ frontend/src/pages/Account/AccountPage.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import HeaderBar from "../../components/HeaderBar/HeaderBar";
+
 export default function AccountPage() {
     // 투어 아이디 불러오기
     const address: string[] = window.location.href.split('/');
     const tourId: string = address[address.length - 2];
 
+    const [tapIdx, setTabIdx] = useState<number>(1);
+
+    const activeStyle = "font-bold color-bg-blue-3";
+
     return (
         <>
-            <h1>가계부 페이지</h1>
-            <p>여행에 대한 가계부를 봅니다.</p>
+            <header>
+                <HeaderBar/>
+            </header>
+            <div className="">
+                <div id='tab-container' className="rounded-lg flex flex-col items-center">
+                    <ul className="w-[90%] grid grid-cols-3 text-sm  text-center">
+                        <li className="me-2" onClick={()=>setTabIdx(1)}>
+                            <div className={`${tapIdx == 1 ? activeStyle : ""} inline-block w-full py-2  rounded-tl-lg`}>개인 지출</div>
+                        </li>
+                        <li className="me-2" onClick={()=>setTabIdx(2)}>
+                            <div className={`${tapIdx == 2 ? activeStyle : ""} inline-block w-full py-2`}>공동 지출</div>
+                        </li>
+                        <li className="me-2"  onClick={()=>setTabIdx(3)}>
+                            <div className={`${tapIdx == 3 ? activeStyle : ""} inline-block w-full py-2 rounded-tr-lg`}>환율</div>
+                        </li>
+                    </ul>
+                    <div className="w-[90%] shadow-lg color-bg-blue-3 rounded-b-lg">
+                        <div>랜딩</div>
+
+                    </div>
+                </div>
+            </div>
             <p>tourId : {tourId}</p>
         </>
     );
