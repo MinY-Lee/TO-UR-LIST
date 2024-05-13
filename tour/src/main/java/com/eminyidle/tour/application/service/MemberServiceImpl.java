62fa26a2095a6562f334f13dce85b73886baca21
이선재 <sj06152@naver.com>
2024-05-14 00:26:40 +0900

    fix: bean 생성 오류 해결

--- tour/src/main/java/com/eminyidle/tour/application/service/MemberServiceImpl.java
+++ tour/src/main/java/com/eminyidle/tour/application/service/MemberServiceImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.UUID;
 
-@Service
+//@Service
 @Slf4j
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
