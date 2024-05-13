6d2b9881f2695a7d81d1230ff153ffdc45529227
김강민 <mmm3657@naver.com>
2024-05-13 23:59:56 +0900

    test: 몽고디비 테스트 로직 수정

--- payment/src/test/java/com/eminyidle/payment/config/CleanUpTestExecutionListener.java
+++ payment/src/test/java/com/eminyidle/payment/config/CleanUpTestExecutionListener.java
@@ -1,6 +1,5 @@
 package com.eminyidle.payment.config;
 
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 import com.eminyidle.payment.repository.PaymentInfoRepository;
@@ -10,11 +9,11 @@ public class CleanUpTestExecutionListener extends AbstractTestExecutionListener 
     @Override
     public void afterTestMethod(TestContext testContext) throws Exception {
         // 테스트 컨텍스트에서 리포지토리를 가져옵니다.
-        PaymentInfoRepository repository = testContext.getApplicationContext()
+        PaymentInfoRepository mongoRepository = testContext.getApplicationContext()
                 .getBean(PaymentInfoRepository.class);
 
         // 데이터베이스에서 모든 데이터를 삭제합니다.
-        repository.deleteAll();
+        mongoRepository.deleteAll();
     }
 
     @Override
