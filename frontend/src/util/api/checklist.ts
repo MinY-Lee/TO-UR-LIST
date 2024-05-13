167885d5733bbaccfc20c4078bcab3ca7a721c31
유혜승 <h103906@naver.com>
2024-05-14 02:49:59 +0900

    feat: 체크리스트 api 테스트

--- frontend/src/util/api/checklist.ts
+++ frontend/src/util/api/checklist.ts
@@ -1,7 +1,16 @@
+import { Item, ItemApi } from "../../types/types";
 import localAxios from "./http-common";
 const local = localAxios();
 
 /** 체크리스트 조회 */
 export async function getChecklist(tourId: string) {
   return await local.get(`/api/checklist/${tourId}`);
+}
+
+/** 체크리스트 추가 */
+export async function addChecklist(type: string, item: ItemApi) {
+  if (type == "private") {
+    return await local.post(`/api/checklist/private`, JSON.stringify(item));
+  }
+  return await local.post(`/api/checklist/public`, JSON.stringify(item));
 }
