2af1fb07020f083293d908485ce4e7e55652f34d
유혜승 <h103906@gmail.com>
2024-05-14 10:22:17 +0900

    feat: 나라 선택 시 맨 위로 스크롤

--- frontend/src/components/CreatePage/setPlace.tsx
+++ frontend/src/components/CreatePage/setPlace.tsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import SearchBar from '../../components/SearchBar/mySearchBar';
 import MyButton from '../../components/Buttons/myButton';
 
-import { City } from '../../types/types';
+import { City, CountryMapping } from '../../types/types';
 import { GetCityList, GetCountryList } from '../../util/api/country';
-
-// import CountryList from '../../dummy-data/get_country.json';
-// import CityList from '../../dummy-data/get_city.json';
+import { HttpStatusCode } from 'axios';
 
 interface PropType {
     onChangeSelected: (selectedCity: City[]) => void;
@@ -18,12 +16,17 @@ export default function SetPlace(props: PropType) {
     const [searchList, setSearchList] = useState<City[]>([]); // 실제 검색 결과
     const [resultList, setResultList] = useState<City[]>([]); // 화면에 보여줄 검색 결과
     const [selectedCity, setSelectedCity] = useState<City[]>([]);
+    const [countryList, setCountryList] = useState<CountryMapping[]>([]);
+    const [countryName, setCountryName] = useState<string>('');
+
+    const topRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        if (topRef.current) {
+            topRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
         // 선택된 도시가 변경될 때마다 resultList 업데이트
-        const updatedResultList = searchList.filter(
-            (city) => !selectedCity.includes(city)
-        );
+        const updatedResultList = searchList.filter((city) => !selectedCity.includes(city));
         setResultList(updatedResultList);
 
         // 부모 컴포넌트에 보내기
@@ -36,40 +39,34 @@ export default function SetPlace(props: PropType) {
         // 나라 -> 도시 로직인 경우 검색어를 나라 코드로 치환
         GetCountryList()
             .then((res) => {
-                // console.log(res);
-                const CountryList = res.data;
-                // console.log(data);
+                if (res.status == HttpStatusCode.Ok) {
+                    setCountryList(res.data);
+                    const foundCountry = countryList.find((country: any) => country.countryName === data);
 
-                const foundCountry = CountryList.find(
-                    (country: any) => country.countryName === data
-                );
-                console.log(foundCountry);
+                    if (foundCountry) {
+                        console.log(foundCountry.countryName);
+                        setCountryName(foundCountry.countryName);
 
-                if (foundCountry) {
-                    // 코드로 도시 검색 및 결과 포맷팅
-                    GetCityList(foundCountry.countryCode).then((res) => {
-                        console.log(res);
-                        const CityList: City[] = [];
-                        res.data.map((cityName: string) => {
-                            const newCity = {
-                                countryCode: foundCountry.countryCode,
-                                cityName: cityName,
-                            };
-                            CityList.push(newCity);
+                        // 코드로 도시 검색 및 결과 포맷팅
+                        GetCityList(foundCountry.countryCode).then((res) => {
+                            const CityList: City[] = [];
+                            res.data.map((cityName: string) => {
+                                const newCity = {
+                                    countryCode: foundCountry.countryCode,
+                                    cityName: cityName,
+                                };
+                                CityList.push(newCity);
+                            });
+
+                            setSearchList(CityList);
+                            // 선택된 도시가 있을 때 결과를 업데이트
+                            const updatedResultList = CityList.filter(
+                                (city) =>
+                                    !selectedCity.some((selected) => JSON.stringify(selected) === JSON.stringify(city))
+                            );
+                            setResultList(updatedResultList);
                         });
-
-                        setSearchList(CityList);
-                        // 선택된 도시가 있을 때 결과를 업데이트
-                        const updatedResultList = CityList.filter(
-                            (city) =>
-                                !selectedCity.some(
-                                    (selected) =>
-                                        JSON.stringify(selected) ===
-                                        JSON.stringify(city)
-                                )
-                        );
-                        setResultList(updatedResultList);
-                    });
+                    }
                 }
             })
             .catch((err) => {
@@ -79,6 +76,7 @@ export default function SetPlace(props: PropType) {
 
     // 여행할 도시 선택 또는 해제
     const handleCitySelect = (city: City) => {
+        // 스크롤 맨 위로
         const index = selectedCity.indexOf(city);
 
         if (index === -1) {
@@ -86,9 +84,7 @@ export default function SetPlace(props: PropType) {
             setSelectedCity([...selectedCity, city]);
         } else {
             // 이미 선택된 도시라면 제거
-            const updatedCities = selectedCity.filter(
-                (selected) => selected !== city
-            );
+            const updatedCities = selectedCity.filter((selected) => selected !== city);
             setSelectedCity(updatedCities);
         }
     };
@@ -96,21 +92,15 @@ export default function SetPlace(props: PropType) {
     return (
         <div className="flex flex-col items-center">
             <div className="text-2xl font-bold m-3">어디로 떠나시나요?</div>
-            <div
-                id="search-container"
-                className="w-[90%] shadow-md border border-black rounded-lg"
-            >
+            <div id="search-container" className="w-[90%] shadow-md border border-black rounded-lg">
                 <SearchBar onChange={handleDataFromChild} />
             </div>
-            <div
-                id="city-list-container"
-                className="m-2 h-[40vh] overflow-scroll w-[90%]"
-            >
+            <div id="city-list-container" className="m-2 h-[40vh] overflow-scroll w-[90%]">
                 {selectedCity.length > 0 &&
                     selectedCity.map((res, index) => (
-                        <div key={index} className="flex justify-between m-2">
+                        <div key={index} className="flex justify-between m-2" ref={topRef}>
                             <div className="text-lg">
-                                {res.countryCode}, {res.cityName}
+                                {countryName}, {res.cityName}
                             </div>
                             <MyButton
                                 type="small"
@@ -122,14 +112,12 @@ export default function SetPlace(props: PropType) {
                         </div>
                     ))}
                 {query !== '' && searchList.length === 0 ? (
-                    <div className="text-lg text-center text-gray-500">
-                        검색 결과가 없습니다.
-                    </div>
+                    <div className="text-lg text-center text-gray-500">검색 결과가 없습니다.</div>
                 ) : (
                     resultList.map((res, index) => (
                         <div key={index} className="flex justify-between m-2">
                             <div className="text-lg">
-                                {res.countryCode}, {res.cityName}
+                                {countryName}, {res.cityName}
                             </div>
                             <MyButton
                                 type="small"
