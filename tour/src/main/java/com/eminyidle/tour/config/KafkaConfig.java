cf3632e68a312e0ceec18b5cab7b2a2baffd6846
이선재 <sj06152@naver.com>
2024-05-15 18:01:45 +0900

    refactor: jsonSerialize -> objectMapper 설정으로 변경

--- tour/src/main/java/com/eminyidle/tour/config/KafkaConfig.java
+++ tour/src/main/java/com/eminyidle/tour/config/KafkaConfig.java
@@ -1,5 +1,7 @@
 package com.eminyidle.tour.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -52,6 +54,14 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
 
+    }
+
+    //ObjectMapper
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); //localDateTime 직렬화 해결
+        return objectMapper;
     }
 
 }
