c6e9b1339b01bf81abceb372fdaba07b28064133
유혜승 <h103906@naver.com>
2024-05-16 04:41:00 +0900

    feat: 나라 검색 api

--- frontend/src/components/TourPage/TourEditHeader.tsx
+++ frontend/src/components/TourPage/TourEditHeader.tsx
@@ -1,20 +1,33 @@
-import { BaseSyntheticEvent, useEffect, useState, useRef } from 'react';
-import { TourInfoDetail, MemberInfo, City, Country } from '../../types/types';
-import MyButton from '../Buttons/myButton';
-import CheckModal from '../CheckModal';
-import MemberAddModal from '../TourPage/AddMemberModal';
-import SearchBar from '../SearchBar/mySearchBar';
+import { BaseSyntheticEvent, useEffect, useState, useRef } from "react";
+import {
+    TourInfoDetail,
+    MemberInfo,
+    City,
+    Country,
+    CountryMapping,
+} from "../../types/types";
+import MyButton from "../Buttons/myButton";
+import CheckModal from "../CheckModal";
+import MemberAddModal from "../TourPage/AddMemberModal";
+import SearchBar from "../SearchBar/mySearchBar";
 
-import MapIcon from '../../assets/svg/mapIcon';
+import MapIcon from "../../assets/svg/mapIcon";
 
-import CountryList from '../../dummy-data/get_country.json';
-// import CityList from "../../dummy-data/get_city.json";
+import CountryList from "../../dummy-data/get_country.json";
 
-import GhostHandleModal from './GhostHandleModal';
-import HostHandleModal from './HostHandleModal';
-import EditMemberList from './EditMemberList';
-import SearchResult from './SearchResult';
-import { editCity, editPeriod, editTitle, getCity, getCountry } from '../../util/api/tour';
+import GhostHandleModal from "./GhostHandleModal";
+import HostHandleModal from "./HostHandleModal";
+import EditMemberList from "./EditMemberList";
+import SearchResult from "./SearchResult";
+import {
+    editCity,
+    editPeriod,
+    editTitle,
+    getCity,
+    getCountry,
+} from "../../util/api/tour";
+import { GetCountryList } from "../../util/api/country";
+import { HttpStatusCode } from "axios";
 
 interface PropType {
     tourId: string;
@@ -25,10 +38,10 @@ interface PropType {
 
 export default function TourEditHeader(props: PropType) {
     const [data, setData] = useState<TourInfoDetail>({
-        tourTitle: '',
+        tourTitle: "",
         cityList: [],
-        startDate: '',
-        endDate: '',
+        startDate: "",
+        endDate: "",
         memberList: [],
     });
     const [hostHandleModal, setHostHandleModal] = useState<boolean>(false);
@@ -38,34 +51,34 @@ export default function TourEditHeader(props: PropType) {
 
     const [memberList, setMemberList] = useState<MemberInfo[]>([]);
     const [deleteMember, setDeleteMember] = useState<MemberInfo>({
-        userId: '',
-        userNickname: '',
-        userName: '',
-        memberType: '',
+        userId: "",
+        userNickname: "",
+        userName: "",
+        memberType: "",
     });
     const [selectedGhostMember, setSelectedGhostMember] = useState<MemberInfo>({
-        userId: '',
-        userNickname: '',
-        userName: '',
-        memberType: '',
+        userId: "",
+        userNickname: "",
+        userName: "",
+        memberType: "",
     });
     const [selectedHostMember, setSelectedHostMember] = useState<MemberInfo>({
-        userId: '',
-        userNickname: '',
-        userName: '',
-        memberType: '',
+        userId: "",
+        userNickname: "",
+        userName: "",
+        memberType: "",
     });
 
     const [searchbarClick, setSearchbarClick] = useState<boolean>(false);
-    const [title, setTitle] = useState<string>('');
-    const [startDate, setStartDate] = useState<string>('');
-    const [endDate, setEndDate] = useState<string>('');
-    const [query, setQuery] = useState<string>('');
+    const [title, setTitle] = useState<string>("");
+    const [startDate, setStartDate] = useState<string>("");
+    const [endDate, setEndDate] = useState<string>("");
+    const [query, setQuery] = useState<string>("");
     const [searchList, setSearchList] = useState<City[]>([]); // 실제 검색 결과
     const [resultList, setResultList] = useState<City[]>([]); // 화면에 보여줄 검색 결과
     const [selectedCity, setSelectedCity] = useState<City[]>([]); // 선택된 도시
 
-    const [countryList, setCountryList] = useState<Country[]>([]);
+    const [countryList, setCountryList] = useState<CountryMapping[]>([]);
     const [cityList, setCityList] = useState<City[]>([]);
 
     const addMemberModalRef = useRef(null);
@@ -73,7 +86,7 @@ export default function TourEditHeader(props: PropType) {
     useEffect(() => {
         setData(props.tourInfo);
 
-        setTitle(data ? data.tourTitle : '');
+        setTitle(data ? data.tourTitle : "");
         setMemberList(data.memberList);
         setStartDate(data.startDate);
         setEndDate(data.endDate);
@@ -82,11 +95,11 @@ export default function TourEditHeader(props: PropType) {
 
     useEffect(() => {
         // 컴포넌트가 마운트될 때 전체 document에 클릭 이벤트 리스너 추가
-        document.addEventListener('click', handleClickOutside);
+        document.addEventListener("click", handleClickOutside);
 
         return () => {
             // 컴포넌트가 언마운트될 때 클릭 이벤트 리스너 제거
-            document.removeEventListener('click', handleClickOutside);
+            document.removeEventListener("click", handleClickOutside);
         };
     }, []);
 
@@ -101,7 +114,11 @@ export default function TourEditHeader(props: PropType) {
     useEffect(() => {
         // 검색 결과를 업데이트
         const updatedResultList = resultList.filter(
-            (res) => !selectedCity.some((selected) => JSON.stringify(selected) === JSON.stringify(res))
+            (res) =>
+                !selectedCity.some(
+                    (selected) =>
+                        JSON.stringify(selected) === JSON.stringify(res)
+                )
         );
         setResultList(updatedResultList);
 
@@ -142,7 +159,7 @@ export default function TourEditHeader(props: PropType) {
             console.log(selectedCity);
             editCity({ tourId: props.tourId, cityList: selectedCity })
                 .then((res) => {
-                    console.log('도시 수정');
+                    console.log("도시 수정");
                     props.onUpdate(true); // 변화 알려
                 })
                 .catch((err) => console.log(err));
@@ -162,17 +179,26 @@ export default function TourEditHeader(props: PropType) {
     // searchBar로부터 데이터 받아서 검색 수행
     const handleQuery = (data: string) => {
         setQuery(data);
-        console.log(data);
         setSearchbarClick(true);
 
         // 나라 -> 도시 로직인 경우 검색어를 나라 코드로 치환
-        const foundCountry = CountryList.find((country) => country.countryName === data);
+        GetCountryList().then((res) => {
+            if (res.status == HttpStatusCode.Ok) {
+                setCountryList(res.data);
+            }
+        });
+
+        const foundCountry = countryList.find(
+            (country) => country.countryName === data
+        );
 
         if (foundCountry) {
             // 코드로 도시 검색 및 결과 포맷팅
             const search: City[] = [];
 
-            const targetCountry = countryList.find((country) => country.countryCode === foundCountry.countryCode);
+            const targetCountry = countryList.find(
+                (country) => country.countryCode === foundCountry.countryCode
+            );
 
             if (targetCountry) {
                 getCity(targetCountry.countryCode)
@@ -192,7 +218,11 @@ export default function TourEditHeader(props: PropType) {
 
             // 선택된 도시가 있을 때 결과를 업데이트
             const updatedResultList = searchList.filter(
-                (city) => !selectedCity.some((selected) => JSON.stringify(selected) === JSON.stringify(city))
+                (city) =>
+                    !selectedCity.some(
+                        (selected) =>
+                            JSON.stringify(selected) === JSON.stringify(city)
+                    )
             );
             setResultList(updatedResultList);
         }
@@ -213,7 +243,9 @@ export default function TourEditHeader(props: PropType) {
             setSelectedCity([...selectedCity, city]);
         } else {
             // 이미 선택된 도시라면 제거
-            const updatedCities = selectedCity.filter((selected) => selected !== city);
+            const updatedCities = selectedCity.filter(
+                (selected) => selected !== city
+            );
             setSelectedCity(updatedCities);
         }
     };
@@ -225,8 +257,13 @@ export default function TourEditHeader(props: PropType) {
 
     const handleClickOutside = (event: Event) => {
         if (addMemberModalRef.current) {
-            const addMemberModalElement = addMemberModalRef.current as HTMLElement;
-            if (addModalClicked && addMemberModalElement && !addMemberModalElement.contains(event.target as Node)) {
+            const addMemberModalElement =
+                addMemberModalRef.current as HTMLElement;
+            if (
+                addModalClicked &&
+                addMemberModalElement &&
+                !addMemberModalElement.contains(event.target as Node)
+            ) {
                 // 모달 영역 외부를 클릭했을 때 모달 닫기
                 setAddModalClicked(false);
             }
@@ -244,7 +281,10 @@ export default function TourEditHeader(props: PropType) {
         setMemberList(updatedList);
     };
 
-    const handleMemberDeleteModal = (member: MemberInfo, event: React.MouseEvent<HTMLDivElement>) => {
+    const handleMemberDeleteModal = (
+        member: MemberInfo,
+        event: React.MouseEvent<HTMLDivElement>
+    ) => {
         event.stopPropagation(); // 이벤트 버블링 중단
         setDeleteMember(member);
         handleMemberDelete();
@@ -275,7 +315,10 @@ export default function TourEditHeader(props: PropType) {
         <div>
             {addModalClicked ? (
                 <div ref={addMemberModalRef} className="h-fit">
-                    <MemberAddModal data={data} closeMemberModal={closeMemberModal} />
+                    <MemberAddModal
+                        data={data}
+                        closeMemberModal={closeMemberModal}
+                    />
                 </div>
             ) : (
                 <></>
@@ -339,7 +382,7 @@ export default function TourEditHeader(props: PropType) {
                             onChange={handleEndDate}
                         ></input>
                     </div>
-                    <div className="flex items-center px-3 w-[90vw] overflow-x-scroll gap-1 h-[7vh]">
+                    <div className="flex items-center px-3 w-[90vw] overflow-x-scroll gap-1">
                         <EditMemberList
                             memberList={memberList}
                             handleGhostModal={handleGhostModal}
@@ -354,8 +397,16 @@ export default function TourEditHeader(props: PropType) {
                             <div className="relative col-span-1 top-[3px]">
                                 <MapIcon />
                             </div>
-                            <div id="search-container" className="col-span-7 justify-center shadow-md rounded-lg">
-                                <div className="text-base" onClick={() => setSearchbarClick(!searchbarClick)}>
+                            <div
+                                id="search-container"
+                                className="col-span-7 justify-center shadow-md rounded-lg"
+                            >
+                                <div
+                                    className="text-base"
+                                    onClick={() =>
+                                        setSearchbarClick(!searchbarClick)
+                                    }
+                                >
                                     <SearchBar onChange={handleQuery} />
                                 </div>
                                 <SearchResult
@@ -373,7 +424,7 @@ export default function TourEditHeader(props: PropType) {
                                     text="적용"
                                     type="full"
                                     className="py-1 font-medium text-white"
-                                    onClick={() => handleTypeChange('default')}
+                                    onClick={() => handleTypeChange("default")}
                                 />
                             </div>
                         </div>
