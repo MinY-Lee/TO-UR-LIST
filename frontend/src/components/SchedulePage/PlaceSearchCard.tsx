dc333a3b407443f09c44c9e68fe404f4a80d361d
김태운 <jim073@naver.com>
2024-05-16 15:51:45 +0900

    fix: 검색 오류 수정

--- frontend/src/components/SchedulePage/PlaceSearchCard.tsx
+++ frontend/src/components/SchedulePage/PlaceSearchCard.tsx
@@ -24,9 +24,9 @@ export default function PlaceSearchCard(props: PropType) {
     const tourId = props.tourId;
 
     return (
-        <div className="w-full h-35vw flex justify-center items-center flex-shrink-0 p-vw">
+        <div className="w-full h-38vw flex justify-center items-center flex-shrink-0 p-vw">
             <div
-                className="w-[95%] p-vw h-full color-border-blue-2 border-halfvw border-rad-2vw flex flex-col"
+                className="w-[95%] p-2vw h-full color-border-blue-2 border-halfvw border-rad-2vw flex flex-col"
                 onClick={() => props.goDetail(props.placeInfo.placeId)}
             >
                 <div className="w-full h-[30%] flex justify-between items-center">
@@ -36,7 +36,7 @@ export default function PlaceSearchCard(props: PropType) {
                     {props.visitedCache.has(props.placeInfo.placeId) ? (
                         //이미 있는 장소면 추가됨
                         <div
-                            className="w-[20%] h-8vw text-5vw border-rad-3vw bg-white color-text-blue-2 color-border-blue-2 border-halfvw flex justify-center items-center"
+                            className="w-[20%] h-7vw text-4vw border-rad-3vw bg-white color-text-blue-2 color-border-blue-2 border-halfvw flex justify-center items-center"
                             onClick={(event) => {
                                 //여행 삭제
                                 if (wsClient) {
@@ -64,7 +64,7 @@ export default function PlaceSearchCard(props: PropType) {
                         </div>
                     ) : (
                         <div
-                            className="w-[20%] h-8vw text-5vw border-rad-3vw color-bg-blue-2 text-white flex justify-center items-center"
+                            className="w-[20%] h-7vw text-4vw border-rad-3vw color-bg-blue-2 text-white flex justify-center items-center"
                             onClick={(event) => {
                                 //추가하는 요청 전송
                                 if (wsClient) {
@@ -92,7 +92,7 @@ export default function PlaceSearchCard(props: PropType) {
                         </div>
                     )}
                 </div>
-                <div className="w-full h-[70%] flex items-center">
+                <div className="w-full h-[70%] flex items-center justify-between">
                     {photoReference !== "" ? (
                         <img
                             crossOrigin="anonymous"
@@ -102,7 +102,7 @@ export default function PlaceSearchCard(props: PropType) {
                     ) : (
                         <div className="w-[30%] h-full aspect-square bg-white border-black border-dot3vw"></div>
                     )}
-                    <div className="w-[70%] h-full p-vw text-4vw">
+                    <div className="w-[68%] h-full p-vw text-4vw">
                         {props.placeInfo.placeAddress}
                     </div>
                 </div>
