aa6235e77ff0ba17c0d0f15ed89192c44f7d2325
김태운 <jim073@naver.com>
2024-05-16 09:38:10 +0900

    proceeding: 구글맵 수정중

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -63,6 +63,7 @@ export default defineConfig({
         headers: {
             "Cross-Origin-Opener-Policy": "same-origin",
             "Cross-Origin-Embedder-Policy": "require-corp",
+            "Cross-Origin-Resource-Policy": "cross-origin",
         },
         proxy: {
             "/api/auth": {
