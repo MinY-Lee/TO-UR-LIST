a1f603a920a7cabd6677fda82bfef45f327fea27
김태운 <jim073@naver.com>
2024-05-18 20:42:28 +0900

    style: css 수정

--- frontend/src/components/SchedulePage/ActivityAddModal.tsx
+++ frontend/src/components/SchedulePage/ActivityAddModal.tsx
@@ -13,6 +13,13 @@ export default function ActivityAddModal(props: PropType) {
     //활동 존재 여부 파악을 위한 hashset
     const [cache, setCache] = useState<Set<string>>(new Set());
 
+    const bgColor = [
+        "color-bg-blue-6",
+        "bg-[#FFB1B1]",
+        "bg-[#FF9315]",
+        "bg-[#5CD651]",
+    ];
+
     useEffect(() => {
         const newSet = new Set<string>();
         props.selectedSchedule.activityList.map((activity: string) => {
@@ -24,17 +31,21 @@ export default function ActivityAddModal(props: PropType) {
     return (
         <>
             <div className="w-full h-full absolute top-0 left-0 z-20 opacity-20 bg-black"></div>
-            <div className="w-[80%] h-[60%] absolute left-[10%] top-[20%] z-30 bg-white color-border-blue-2 border-halfvw border-rad-2vw flex flex-col justify-around items-center p-2vw">
+            <div className="w-[80%] h-[60%] absolute left-[10%] top-[20%] z-30 bg-white color-border-blue-6 border-halfvw border-rad-2vw flex flex-col justify-around items-center p-2vw overflow-y-scroll">
                 {/* 현재 추가된 활동 */}
                 <div className="w-full flex flex-col">
-                    <div className="text-7vw">현재 추가된 활동</div>
-                    <div className="w-full text-5vw">
+                    <div className="text-7vw weight-text-semibold">
+                        현재 추가된 활동
+                    </div>
+                    <div className="w-full text-5vw break-keep flex flex-wrap">
                         {props.selectedSchedule.activityList.length > 0 ? (
                             props.selectedSchedule.activityList.map(
-                                (activity) => {
+                                (activity, index) => {
                                     return (
-                                        <span
-                                            className="px-2vw border-halfvw color-border-blue-2 color-bg-blue-2 text-white border-rad-3vw mx-vw"
+                                        <div
+                                            className={`px-2vw mb-vw text-white border-rad-2dot5vw mr-vw flex-shrink-0 ${
+                                                bgColor[index % 4]
+                                            }`}
                                             onClick={() => {
                                                 props.deleteActivity(
                                                     props.selectedSchedule
@@ -44,7 +55,7 @@ export default function ActivityAddModal(props: PropType) {
                                             }}
                                         >
                                             {"- " + activity}
-                                        </span>
+                                        </div>
                                     );
                                 }
                             )
@@ -55,12 +66,14 @@ export default function ActivityAddModal(props: PropType) {
                 </div>
                 {/* 추천 활동 */}
                 <div className="w-full flex flex-col">
-                    <div className="text-5vw">이 장소에서 추천하는 활동</div>
+                    <div className="text-5vw weight-text-semibold">
+                        이 장소에서 추천하는 활동
+                    </div>
                     <div className="flex items-center text-5vw text-[#afafaf]">
                         {props.activityList.length > 0 ? (
                             cache.has(props.activityList[0]) ? (
                                 <span
-                                    className="px-2vw border-halfvw color-border-blue-2 color-bg-blue-2 text-white border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw color-border-blue-6 color-bg-blue-6 text-white border-rad-2dot5vw mr-vw"
                                     onClick={() => {
                                         props.deleteActivity(
                                             props.selectedSchedule.tourPlaceId,
@@ -72,7 +85,7 @@ export default function ActivityAddModal(props: PropType) {
                                 </span>
                             ) : (
                                 <span
-                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-2dot5vw mr-vw"
                                     onClick={() =>
                                         props.addActivity(
                                             props.selectedSchedule.placeId,
@@ -90,7 +103,7 @@ export default function ActivityAddModal(props: PropType) {
                         {props.activityList.length > 1 ? (
                             cache.has(props.activityList[1]) ? (
                                 <span
-                                    className="px-2vw border-halfvw color-border-blue-2 color-bg-blue-2 text-white border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw color-border-blue-6 color-bg-blue-6 text-white border-rad-2dot5vw mr-vw"
                                     onClick={() => {
                                         props.deleteActivity(
                                             props.selectedSchedule.tourPlaceId,
@@ -102,7 +115,7 @@ export default function ActivityAddModal(props: PropType) {
                                 </span>
                             ) : (
                                 <span
-                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-2dot5vw mr-vw"
                                     onClick={() =>
                                         props.addActivity(
                                             props.selectedSchedule.placeId,
@@ -120,7 +133,7 @@ export default function ActivityAddModal(props: PropType) {
                         {props.activityList.length > 2 ? (
                             cache.has(props.activityList[2]) ? (
                                 <span
-                                    className="px-2vw border-halfvw color-border-blue-2 color-bg-blue-2 text-white border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw color-border-blue-6 color-bg-blue-6 text-white border-rad-2dot5vw mr-vw"
                                     onClick={() => {
                                         props.deleteActivity(
                                             props.selectedSchedule.tourPlaceId,
@@ -132,7 +145,7 @@ export default function ActivityAddModal(props: PropType) {
                                 </span>
                             ) : (
                                 <span
-                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-3vw mx-vw"
+                                    className="px-2vw border-halfvw border-[#afafaf] border-rad-2dot5vw mr-vw"
                                     onClick={() =>
                                         props.addActivity(
                                             props.selectedSchedule.placeId,
@@ -152,14 +165,21 @@ export default function ActivityAddModal(props: PropType) {
 
                 {/* 전체 보기 */}
                 <div className="w-full h-[40%] flex flex-col">
-                    <div className="text-5vw h-[20%]">전체 선택 가능 활동</div>
-                    <div className="text-5vw h-[80%] overflow-y-scroll">
+                    <div className="text-5vw h-5vw weight-text-semibold">
+                        전체 선택 가능 활동
+                    </div>
+                    <div
+                        className="text-5vw overflow-y-scroll"
+                        style={{
+                            height: "calc(100% - 5vw)",
+                        }}
+                    >
                         <div className="flex items-center text-5vw text-[#afafaf] flex-wrap">
                             {props.activityList.length > 0 ? (
                                 props.activityList.map((activity) => {
                                     return cache.has(activity) ? (
                                         <span
-                                            className="px-2vw border-halfvw color-border-blue-2 color-bg-blue-2 text-white border-rad-3vw m-vw flex-shrink-0"
+                                            className="px-2vw border-halfvw color-border-blue-6 color-bg-blue-6 text-white border-rad-2dot5vw m-vw flex-shrink-0"
                                             onClick={() => {
                                                 props.deleteActivity(
                                                     props.selectedSchedule
@@ -172,7 +192,7 @@ export default function ActivityAddModal(props: PropType) {
                                         </span>
                                     ) : (
                                         <span
-                                            className="px-2vw border-halfvw border-[#afafaf] border-rad-3vw m-vw flex-shrink-0"
+                                            className="px-2vw border-halfvw border-[#afafaf] border-rad-2dot5vw m-vw flex-shrink-0"
                                             onClick={() =>
                                                 props.addActivity(
                                                     props.selectedSchedule
@@ -195,7 +215,7 @@ export default function ActivityAddModal(props: PropType) {
                 </div>
                 <div className="w-full h-[10%] flex justify-around items-center text-5vw">
                     <div
-                        className="w-full h-full color-bg-blue-2 text-white border-rad-2vw flex justify-center items-center"
+                        className="w-full h-full color-bg-blue-6 text-white border-rad-2vw flex justify-center items-center"
                         onClick={props.closeModal}
                     >
                         닫기
